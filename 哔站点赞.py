1b7b3e7305ee6b3264c107765ebe9f47f97bf33a
王幸凤 <152585733+wangxingfen@users.noreply.github.com>
2025-10-16 23:16:17 +0800

    Add files via upload

--- 哔站点赞.py
+++ 哔站点赞.py
@@ -1,13 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import Byt
 import time
 import random
 import json
-from openai import OpenAI
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
